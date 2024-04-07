217f2eac333bf361da1b3067edf9e036c1d86053
jliu <ecnucrystal@gmail.com>
2024-04-07 20:54:23 +0200

    update data preprocessing

--- data_preparation.py
+++ data_preparation.py
@@ -1,3 +1,22 @@
+# standard imports
 """
 preprocess infant-directed speech transcripts as the train data
 """
+################# Libraries #################
+import os
+
+################# Functions #################
+
+
+################# Main #################
+if __name__ == '__main__':
+
+    # concatenate results into a txt format
+    text_path = '/data/Generative_replay/knn-transformers/data/'
+    for text in os.listdir(text_path):
+
+        #
+
+    # clean train data
+
+
---
+++ rank_h.py
@@ -0,0 +1,9 @@
+"""
+rank the prompts based on entropy (to align with the knn-lM model)
+
+input: train data (sliding window)
+model: the trained opt model
+return: a list of ranked prompts
+"""
+
+
